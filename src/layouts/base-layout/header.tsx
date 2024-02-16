18881d106ca49597874bdbaf0a166fed23fb69bd
Pavel Shapov <pavel.shapov@metalamp.io>
2024-02-16 16:17:04 +0600

    feat: landing page

--- src/layouts/base-layout/header.tsx
+++ src/layouts/base-layout/header.tsx
@@ -1,12 +1,50 @@
 "use client";
+
+import cn from "classnames";
+import { usePathname } from "next/navigation";
 import type { FC } from "react";
 
 import { WalletButton } from "@promo-shock/components";
+import { Link, Logo, TabLink } from "@promo-shock/ui-kit";
 
-const Header: FC = () => {
+import styles from "./base-layout.module.scss";
+
+type Props = {
+  isMirror?: boolean;
+  gutterBottom?: boolean;
+};
+
+const Header: FC<Props> = ({ isMirror, gutterBottom }) => {
+  const pathname = usePathname();
   return (
-    <div>
-      <WalletButton />
+    <div
+      className={cn(
+        styles.header,
+        { [styles.mirror]: isMirror },
+        { [styles.gutterBottom]: gutterBottom },
+      )}
+    >
+      <div className={styles.container}>
+        <Link href="/">
+          <Logo />
+        </Link>
+
+        <div className={styles.rightPart}>
+          <nav className={styles.nav}>
+            <TabLink
+              active={pathname.includes("streams")}
+              href="/streams"
+              text="Streams"
+            />
+            <TabLink
+              active={pathname.includes("promos")}
+              href="/promos"
+              text="Promo"
+            />
+          </nav>
+          <WalletButton />
+        </div>
+      </div>
     </div>
   );
 };
--- src/ui-kit/index.ts
+++ src/ui-kit/index.ts
@@ -5,4 +5,8 @@ export * from "./marquee";
 export * from "./segmented";
 export * from "./breadcrumb";
 export * from "./card";
+export * from "./logo";
+export * from "./tabs";
+export * from "./stream-card";
 export * from "./uploaders";
+export * from "./tooltips";
